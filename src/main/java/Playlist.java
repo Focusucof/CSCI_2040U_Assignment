e76dae00eb6cd0140166085705bc8c41fffca172
Devin Boodoo <devinoboodoo@gmail.com>
2026-03-06 16:21:11 -0500

    feat: playlist class added

---
+++ src/main/java/Playlist.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class Playlist {
+    private int id;
+    private String name;
+    private String ownerId;
+    private int[] songIds;
+    private Date createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int[] getSongIds() {
+        return songIds;
+    }
+
+    public void setSongIds(int[] songIds) {
+        this.songIds = songIds;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Playlist(int id, String name, String ownerId, int[] songIds, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.ownerId = ownerId;
+        this.songIds = songIds;
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,4 +4,50 @@ public class User {
     private String password;
     private Playlist likedSongs;
     private Playlist[] playlists;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Playlist getLikedSongs() {
+        return likedSongs;
+    }
+
+    public void setLikedSongs(Playlist likedSongs) {
+        this.likedSongs = likedSongs;
+    }
+
+    public Playlist[] getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(Playlist[] playlists) {
+        this.playlists = playlists;
+    }
+
+    public User(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 }
